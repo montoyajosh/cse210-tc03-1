cac0f549ddab860c8398b1e0b531dfe164c863a6
montoyajosh <mon21012@byui.edu>
2021-09-28 07:27:07 +0800

    commit

--- python_code/main.py
+++ python_code/main.py
@@ -1,4 +1,4 @@
-import zwane, jerry
+import zwane, jerry, joshua
 
 # Main starts the program
 def main():
@@ -8,6 +8,7 @@ def main():
     # Print  greetings
     zwane.display_greeting()
     jerry.greeting()
+    joshua.greeting()
 
 # Start main
 if __name__ == "__main__":
